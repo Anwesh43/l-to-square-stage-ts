349537b6879167fa2d76db72c520c413f1cb44f7
Anwesh43 <anweshthecool0@gmail.com>
2019-02-07 10:47:13 +0530

    adding constants and methods

---
+++ LToSquareStage.ts
@@ -0,0 +1,28 @@
+const w : number = window.innerWidth
+const h : number = window.innerHeight
+const nodes : number = 5
+const lines : number = 2
+const scGap : number = 0.05
+const scDiv : number = 0.51
+const sizeFactor : number = 3
+const strokeFactor : number = 90
+const foreColor : string = "#0277BD"
+const backColor : string = "#212121"
+
+const maxScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.max(0, scale - i / n)
+}
+
+const divideScale : Function = (scale : number, i : number, n : number) : number => {
+    return Math.min(1 / n, maxScale(i, n)) * n
+}
+const scaleFactor : Function = (scale : number) : number => Math.floor(scale / scDiv)
+
+const mirrorValue : Function = (scale : number, a : number, b : number) : number => {
+    const k = scaleFactor(scale)
+    return (1 - k) / a + k / b
+}
+
+const updateValue : Function = (scale : number, dir : number, a : number, b : number) => {
+    return mirrorValue(scale, a, b) * dir * scGap
+}
