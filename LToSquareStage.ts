0b2ebb08856af57027e38c0e3a28d05348cdff17
Anwesh43 <anweshthecool0@gmail.com>
2019-02-08 11:05:11 +0530

    adding LToSquareStage class

--- LToSquareStage.ts
+++ LToSquareStage.ts
@@ -26,3 +26,33 @@ const mirrorValue : Function = (scale : number, a : number, b : number) : number
 const updateValue : Function = (scale : number, dir : number, a : number, b : number) => {
     return mirrorValue(scale, a, b) * dir * scGap
 }
+
+class LToSquareStage {
+    canvas : HTMLCanvasElement = document.createElement('canvas')
+    context : CanvasRenderingContext2D
+
+    initCanvas() {
+        this.canvas.width = w
+        this.canvas.height = h
+        this.context = this.canvas.getContext('2d')
+        document.body.appendChild(this.canvas)
+    }
+
+    render() {
+        this.context.fillStyle = backColor
+        this.context.fillRect(0, 0, w, h)
+    }
+
+    handleTap() {
+        this.canvas.onmousedown  = () => {
+
+        }
+    }
+
+    static init() {
+        const stage : LToSquareStage = new LToSquareStage()
+        stage.initCanvas()
+        stage.render()
+        stage.handleTap()
+    }
+}
