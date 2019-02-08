cf6ece24bca04a72d3df46fe57d7f23521c3636d
Anwesh43 <anweshthecool0@gmail.com>
2019-02-08 15:10:19 +0530

    adding LTSNode class

--- LToSquareStage.ts
+++ LToSquareStage.ts
@@ -129,3 +129,47 @@ class Animator {
         }
     }
 }
+
+class LTSNode {
+    next : LTSNode
+    prev : LTSNode
+    state : State = new State()
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new LTSNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        drawLTSNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : LTSNode {
+        var curr : LTSNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr != null) {
+            return curr
+        }
+        cb()
+        return this
+    }
+}
